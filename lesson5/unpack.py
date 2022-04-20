d7e97fc79ea8e634af4897050e8c451fc822e004
Ignisor <Ignis2497@gmail.com>
2022-04-20 22:06:26 +0300

    Update unpack.py

--- lesson5/unpack.py
+++ lesson5/unpack.py
@@ -13,6 +13,14 @@ print(peach)
 print(banana)
 print(apple)
 
+fruits = ('🍑', '🍌', '🍏', '🍊', '🍉')
+peach, banana, apple, *other_fruits = fruits
+
+print(peach)
+print(banana)
+print(apple)
+print(other_fruits)
+
 peach, banana = banana, peach
 peach, banana = (banana, peach)
 print(peach, banana)
