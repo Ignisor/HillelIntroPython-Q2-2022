01852ebd52239a1c35b1a5f387ff7b4019600abf
Ignisor <Ignis2497@gmail.com>
2022-05-07 14:10:36 +0300

    Update utils_file.py

--- lesson8/utils_file.py
+++ lesson8/utils_file.py
@@ -19,3 +19,10 @@ def generate_cucumber():
         cucumber += char
 
     return cucumber
+
+
+print(__name__)  # цей код та код до цього буде виконуватись коли ми імпортуємо файл
+
+if __name__ == '__main__':  # ця частина коду буде виконуватись лише якщо ми запускаємо саме цей файл
+    print(__name__)
+    print(generate_cucumber())
